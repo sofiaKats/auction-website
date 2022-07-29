5f1bd24f01dd592f33f297bfa4a19bad5ef732d1
Sophia Katsaouni <76869852+sofiaKats@users.noreply.github.com>
2022-07-29 14:54:41 +0300

    Delete website directory

--- website/ts-hello/main.js
+++
@@ -1,5 +0,0 @@
-function log(message) {
-    console.log(message);
-}
-var message = 'Hello World';
-log(message);
--- website/ts-hello/main.ts
+++
@@ -1,9 +0,0 @@
-function doSomething() {
-    for(var i = 0; i < 5; i++ ) {
-        console.log(i);
-    }
-
-    console.log('Finally: ' + i);
-}
-
-doSomething();
