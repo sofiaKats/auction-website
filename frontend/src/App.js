a7fe674ef493a3f7a30b1ef57dbedcccde3c127f
sofiaKats <sophiakatsaouni@gmail.com>
2022-07-30 01:50:47 +0300

    have to check navigate function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import React from 'react';
 import NavBar from './components/Homepage/NavbarComponent.jsx';
 import Footer from './components/Homepage/FooterComponent.jsx';
 import Home from './components/Homepage/HomeComponent.jsx';
-import Auth from './components/Homepage/AuthComponent.jsx'
+import LogInComponent from './components/SignUp-LogIn/LogInComponent';
+import SignUpComponent from './components/SignUp-LogIn/SignUpComponent';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 // this file is where the paths(routes) of each component exist
@@ -16,7 +17,8 @@ function App() {
             <div className='container-fluid'>
               <Routes>
                 <Route path='/' element={<Home/>} />
-                <Route path='/Auth' element={<Auth/>} />
+                <Route path = "/LogIn" component = {LogInComponent} />
+                <Route path = "/SignUp" component = {SignUpComponent} />
               </Routes>
             </div>
           <Footer />
--- frontend/src/components/Homepage/NavbarComponent.jsx
+++ frontend/src/components/Homepage/NavbarComponent.jsx
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
-
+// import { withParamsAndNavigate } from "./getParamsAndNavigate.js";
+import { useNavigate } from 'react-router-dom';
 
 class NavbarComponent extends Component {
     constructor(props) {
         super(props)
+        // const navigate = useNavigate();
 
         this.state = {
                  
         }
+        this.SignUp = this.SignUp.bind(this);
+    }
+
+    SignUp() {
+        // this.props.history.push('/SignUp');
+        // navigate('/SignUp');
     }
     
     render() {
+        const{params,navigate} = this.props;
         return (
             <div>
                 <header>
                     <nav className="navbar navbar-expand navbar-dark bg-dark">
-                        <div><a href="#" className="navbar-brand">Auction Bay</a></div>
+                        <div><a href="https://javaguides.net" className="navbar-brand">Auction Bay</a></div>
                         <div class="col-sm-10" align="right">
-                            <button class="button" type="button" >Sign Up</button>
+                            <button class="button" type="button"onClick={this.SignUp} >Sign Up</button>
                             <button class="btn btn-outline-light my-2 my-sm-0" type="button">Log In</button>
                         </div>
                     </nav>
