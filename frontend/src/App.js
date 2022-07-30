bfd746c81d786fb2a61b0cce934fc5123d882d83
sofiaKats <sophiakatsaouni@gmail.com>
2022-07-30 18:07:39 +0300

    login form all done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import React from 'react';
 // import Home from './components/Homepage/HomeComponent.jsx';
-// import LogInComponent from './components/SignUp-LogIn/LogInComponent';
-// import SignUpComponent from './components/SignUp-LogIn/SignUpComponent';
+import LogInComponent from './components/SignUp-LogIn/LogInComponent';
+import SignUpComponent from './components/SignUp-LogIn/SignUpComponent';
 import {BrowserRouter as Router, Route, Routes,  Navigate} from 'react-router-dom';
 import HomeComponent from './components/Homepage/HomeComponent.jsx';
-import Auth from './components/SignUp-LogIn/Auth'
 
 // this file is where the paths(routes) of each component exist
 function App() {
@@ -15,8 +14,8 @@ function App() {
             <div className='container-fluid'>
               <Routes>
                 <Route path='/' element={<HomeComponent/>} />
-                <Route path = "/LogIn" element={<Auth/>} />
-                <Route path = "/SignUp" element={<Auth/>} />
+                <Route path = "/LogIn" element={<LogInComponent/>} />
+                <Route path = "/SignUp" element={<SignUpComponent/>} />
               </Routes>
             </div>
       </Router>
