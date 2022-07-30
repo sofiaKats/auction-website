cb2758524ee57a2214a80f6c8f51b76e61fb4687
sofiaKats <sophiakatsaouni@gmail.com>
2022-07-30 15:11:42 +0300

    button redirection all done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,8 @@ import Footer from './components/Homepage/FooterComponent.jsx';
 import Home from './components/Homepage/HomeComponent.jsx';
 import LogInComponent from './components/SignUp-LogIn/LogInComponent';
 import SignUpComponent from './components/SignUp-LogIn/SignUpComponent';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes,  Navigate} from 'react-router-dom';
+import HomeComponent from './components/Homepage/HomeComponent.jsx';
 
 // this file is where the paths(routes) of each component exist
 function App() {
@@ -16,9 +17,9 @@ function App() {
             <NavBar />
             <div className='container-fluid'>
               <Routes>
-                <Route path='/' element={<Home/>} />
+                <Route path='/' element={<HomeComponent/>} />
                 <Route path = "/LogIn" component = {LogInComponent} />
-                <Route path = "/SignUp" component = {SignUpComponent} />
+                <Route path = "/SignUp" element={<SignUpComponent/>} />
               </Routes>
             </div>
           <Footer />
--- frontend/src/components/Homepage/NavbarComponent.jsx
+++ frontend/src/components/Homepage/NavbarComponent.jsx
@@ -1,39 +1,35 @@
 import React, { Component } from 'react';
-// import { withParamsAndNavigate } from "./getParamsAndNavigate.js";
-import { useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 class NavbarComponent extends Component {
+    
     constructor(props) {
         super(props)
-        // const navigate = useNavigate();
 
         this.state = {
                  
         }
-        this.SignUp = this.SignUp.bind(this);
-    }
-
-    SignUp() {
-        // this.props.history.push('/SignUp');
-        // navigate('/SignUp');
     }
     
     render() {
-        const{params,navigate} = this.props;
+        const { navigation } = this.props;
         return (
             <div>
                 <header>
                     <nav className="navbar navbar-expand navbar-dark bg-dark">
                         <div><a href="https://javaguides.net" className="navbar-brand">Auction Bay</a></div>
                         <div class="col-sm-10" align="right">
-                            <button class="button" type="button"onClick={this.SignUp} >Sign Up</button>
-                            <button class="btn btn-outline-light my-2 my-sm-0" type="button">Log In</button>
+                            <Link to="/SignUp" class="button">Sign up</Link>
+                            <Link to="/LogIn" class="btn btn-outline-light my-2 my-sm-0">Log In</Link>
+                            {/* <button class="button" type="button" >Sign Up</button> */}
+                            {/* <button class="btn btn-outline-light my-2 my-sm-0" type="button">Log In</button> */}
                         </div>
                     </nav>
                 </header>
             </div>
         );
     }
+
 }
 
 export default NavbarComponent;
