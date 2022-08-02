0774d51b7da497adce99812b02b17f68a8ce1ba8
sofiaKats <sophiakatsaouni@gmail.com>
2022-08-02 19:03:21 +0300

    deletion of useless import

--- backend/src/main/java/com/project/backend/User/UserService.java
+++ backend/src/main/java/com/project/backend/User/UserService.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
