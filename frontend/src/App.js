77e4ef69901bfd59da0496f768c244427635a31f
sofiaKats <sophiakatsaouni@gmail.com>
2022-08-07 14:22:18 +0300

    commiting files after some npm installs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import LogInComponent from './components/SignUp-LogIn/LogInComponent';
 import SignUpComponent from './components/SignUp-LogIn/SignUpComponent';
 import {BrowserRouter as Router, Route, Routes,  Navigate} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import WelcomeComponent from './components/WelcomePage/WelcomeComponent.jsx';
 
 // this file is where the paths(routes) of each component exist
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 
