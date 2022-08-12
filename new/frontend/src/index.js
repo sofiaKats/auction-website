f884dfed462cd9ddfcf0cdc660604e730d1452ef
sofiaKats <sophiakatsaouni@gmail.com>
2022-08-13 02:46:05 +0300

    ReactDOM.render is outdated for react 18 so changed that

--- new/frontend/src/index.js
+++ new/frontend/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  // document.getElementById("root")
 );
 
 serviceWorker.unregister();
