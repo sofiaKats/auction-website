2e21e1223c8f6d18e4bfc5334ab47b33c97c218b
sofiaKats <sophiakatsaouni@gmail.com>
2022-08-12 03:26:21 +0300

    deletion of moderator once and for all

--- new/frontend/src/services/test.service.js
+++ new/frontend/src/services/test.service.js
@@ -11,10 +11,6 @@ const getUserBoard = () => {
   return axios.get(API_URL + "user", { headers: authHeader() });
 };
 
-const getModeratorBoard = () => {
-  return axios.get(API_URL + "mod", { headers: authHeader() });
-};
-
 const getAdminBoard = () => {
   return axios.get(API_URL + "admin", { headers: authHeader() });
 };
@@ -22,7 +18,6 @@ const getAdminBoard = () => {
 const TestService = {
   getPublicContent,
   getUserBoard,
-  getModeratorBoard,
   getAdminBoard,
 };
 
