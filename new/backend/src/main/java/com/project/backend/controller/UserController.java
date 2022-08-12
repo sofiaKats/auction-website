5ee080df62892a3ae6a7038d1b4325adb5e3a75b
sofiaKats <sophiakatsaouni@gmail.com>
2022-08-13 02:45:15 +0300

    addition of permitAll @ configure funct for the rest of the new possible paths

--- new/backend/src/main/java/com/project/backend/controller/UserController.java
+++ new/backend/src/main/java/com/project/backend/controller/UserController.java
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
