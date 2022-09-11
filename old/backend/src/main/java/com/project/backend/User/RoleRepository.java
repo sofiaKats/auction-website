d59bbcd3b72299e798d23c3f662a718ae6298701
sofiaKats <sophiakatsaouni@gmail.com>
2022-09-11 20:53:31 +0300

    deletion of old folder

--- old/backend/src/main/java/com/project/backend/User/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.backend.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- old/backend/src/main/java/com/project/backend/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
